5c57aa50813fed3e2047c7b6c051e456e9552f5a
GustavoBarros360 <gustavo_barros@usp.br>
2021-09-09 23:31:46 -0300

    login mutation feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import { useState } from 'react';
+import { gql, useMutation, ApolloClient, InMemoryCache } from '@apollo/client';
 import './App.css';
+
+export const client = new ApolloClient({
+  uri: 'https://tq-template-server-sample.herokuapp.com/graphql',
+  cache: new InMemoryCache()
+});
 
 function App() {
 
@@ -27,6 +33,7 @@ function App() {
       alert('Inform a valid email');
       return false;
     }
+    return true;
   }
 
   const validatePassword = (password: string) => {
@@ -47,13 +54,58 @@ function App() {
       alert('Password must have at least one digit and one letter');
       return false;
     }
+    return true;
   }
 
-  const validateForm = () => {
+  async function validForm() {
+    return (validateEmail(data.email) && validatePassword(data.password));
+  }
+
+  async function validateForm  (event: any){
+    event.preventDefault();
     validateEmail(data.email);
     validatePassword(data.password);
+    await loginMutation(data.email, data.password);
   }
 
+  const LOGIN_MUTATION = gql`
+    mutation login(
+      $email: String!,
+      $password: String!
+    ) {
+      login(
+        data: {
+          email: $email
+          password: $password
+        }
+      ) {
+        token
+      }
+    } 
+  `;
+
+  const [login, { loading }] = useMutation(LOGIN_MUTATION);
+
+  if (loading) {
+    console.log('Loading...');
+  }
+
+  const loginMutation = async (email: string, password: string) => {
+
+    if (await validForm()) {
+      try {
+        const response = await login({
+          variables: {
+            email: email,
+            password: password
+          }
+        })
+        localStorage.setItem(data.email, response.data.login.token);
+      } catch (error) {
+      alert(error);
+      } 
+    } 
+  }
   return (
     <div className='App'>
       <header className='App-header'>
