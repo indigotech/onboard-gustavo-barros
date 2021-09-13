755e385712a05d08359cd5697e57b226e2902429
GustavoBarros360 <gustavo_barros@usp.br>
2021-09-13 10:24:00 -0300

    fixing indentation and function names

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,18 @@ import { validatePassword, validateEmail } from './validations/validations';
 import { LOGIN_MUTATION } from './GraphQL/mutations/mutations';
 import './App.css';
 
-
 function App(): JSX.Element {
-
   const [data, setData] = useState({
     email: '',
     password: '',
-  })
+  });
 
-  const handleInputChange = (event: { target: { name: string; value: string; }; }) => {
+  const handleInputChange = (event: { target: { name: string; value: string } }) => {
     setData({
       ...data,
-      [event.target.name] : event.target.value
-    })
-  }
-
-  function validForm() {
-    return (validateEmail(data.email) && validatePassword(data.password));
-  }
-
-  async function submitForm (event: any){
-    event.preventDefault();
-    await loginMutation(data.email, data.password);
-  }
+      [event.target.name]: event.target.value,
+    });
+  };
 
   const [login, { loading }] = useMutation(LOGIN_MUTATION);
 
@@ -34,45 +23,43 @@ function App(): JSX.Element {
     console.log('Loading...');
   }
 
-  const loginMutation = async (email: string, password: string) => {
+  function isFormValid() {
+    return validateEmail(data.email) && validatePassword(data.password);
+  }
 
-    if (validForm()) {
+  async function submitForm(event: any) {
+    event.preventDefault();
+    await loginMutation(data.email, data.password);
+  }
+
+  const loginMutation = async (email: string, password: string) => {
+    if (isFormValid()) {
       try {
         const response = await login({
           variables: {
             email: email,
-            password: password
-          }
-        })
+            password: password,
+          },
+        });
         localStorage.setItem(data.email, response.data.login.token);
       } catch (error) {
-      alert(error);
-      } 
-    } 
-  }
+        alert(error);
+      }
+    }
+  };
   return (
-    
-      <div className='App'>
-        <header className='App-header'>
-          <form className='App-form'>
-            <h1>Bem vindo à Taqtile!</h1>
-            <label>Email</label>
-            <input
-              type='text'
-              name='email'
-              className='Input'
-              onChange={handleInputChange} />
-            <label>Senha</label>
-            <input
-              type='text'
-              name='password'
-              className='Input'
-              onChange={handleInputChange} />
-            <input type='submit' value='Entrar' className='Submit-button' onClick={submitForm} />
-          </form>
-        </header>
-      </div>
-    
+    <div className='App'>
+      <header className='App-header'>
+        <form className='App-form'>
+          <h1>Bem vindo à Taqtile!</h1>
+          <label>Email</label>
+          <input type='text' name='email' className='Input' onChange={handleInputChange} />
+          <label>Senha</label>
+          <input type='text' name='password' className='Input' onChange={handleInputChange} />
+          <input type='submit' value='Entrar' className='Submit-button' onClick={submitForm} />
+        </form>
+      </header>
+    </div>
   );
 }
 
--- src/GraphQL/mutations/mutations.tsx
+++ src/GraphQL/mutations/mutations.tsx
@@ -1,22 +1,14 @@
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 
 export const client = new ApolloClient({
-    uri: 'https://tq-template-server-sample.herokuapp.com/graphql',
-    cache: new InMemoryCache()
-  });
-  
+  uri: 'https://tq-template-server-sample.herokuapp.com/graphql',
+  cache: new InMemoryCache(),
+});
+
 export const LOGIN_MUTATION = gql`
-  mutation login(
-    $email: String!,
-    $password: String!
-  ) {
-    login(
-      data: {
-        email: $email
-        password: $password
-      }
-    ) {
+  mutation login($email: String!, $password: String!) {
+    login(data: { email: $email, password: $password }) {
       token
     }
-  } 
-  `;
+  }
+`;
