f76fb4ad753059d7d51dcb9513d4a81bd82f9b72
GustavoBarros360 <gustavo_barros@usp.br>
2021-09-17 23:44:20 -0300

    create users mutation implemented

--- src/components/add-user-page/AddUserPage.tsx
+++ src/components/add-user-page/AddUserPage.tsx
@@ -1,9 +1,10 @@
 import { useMutation } from '@apollo/client';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import ClipLoader from 'react-spinners/ClipLoader';
 import '../../App.css';
 import { CREATE_USER } from '../../GraphQL/mutations/mutations';
-import { validateEmail, validateName, validatePhone } from '../../validations/validations';
+import { validateEmail, validateName, validatePassword, validatePhone } from '../../validations/validations';
 import { usersListPath } from '../users-list/UsersList';
 
 function AddUserPage(): JSX.Element {
@@ -12,14 +13,12 @@ function AddUserPage(): JSX.Element {
   const [birthDate, setBirthDate] = useState('');
   const [email, setEmail] = useState('');
   const [role, setRole] = useState('');
-  const [createUser, { loading, error }] = useMutation(CREATE_USER);
+  const [password, setPassword] = useState('');
+  const [canRedirect, setCanRedirect] = useState(false);
+  const [createUser, { loading }] = useMutation(CREATE_USER);
 
   if (loading) {
-    console.log('loading');
-  }
-
-  if (error) {
-    alert(`Erro de teste: ${error.message}`);
+    return <ClipLoader color={'#000'} size={25} />;
   }
 
   const handleNameChange = (event: { target: { value: string } }) => {
@@ -28,7 +27,7 @@ function AddUserPage(): JSX.Element {
   const handlePhoneChange = (event: { target: { value: string } }) => {
     setPhone(event.target.value);
   };
-  const handleBirthDateChange = (event: { target: { value: any } }) => {
+  const handleBirthDateChange = (event: { target: { value: string } }) => {
     setBirthDate(event.target.value);
   };
   const handleEmailChange = (event: { target: { value: string } }) => {
@@ -37,29 +36,37 @@ function AddUserPage(): JSX.Element {
   const handleRoleChange = (event: { target: { value: string } }) => {
     setRole(event.target.value);
   };
+  const handlePasswordChange = (event: { target: { value: string } }) => {
+    setPassword(event.target.value);
+  };
 
-  async function submitForm() {
-    validateName(name);
-    validatePhone(phone);
-    validateEmail(email);
-    await newUser(name, email, phone, birthDate, role);
+  function isFormValid() {
+    return validateName(name) && validatePhone(phone) && validateEmail(email) && validatePassword(password);
   }
 
-  async function newUser(name: string, email: string, phone: string, birthDate: string, role: string) {
-    try {
-      const response = await createUser({
-        variables: {
-          name: name,
-          email: email,
-          phone: phone,
-          birthDate: birthDate,
-          password: 'dvidhqsaihb76437824',
-          //role: role,
-        },
-      });
-      console.log('response', response);
-    } catch (error) {
-      alert(error);
+  async function submitForm() {
+    await newUser(name, email, phone, birthDate, role, password);
+  }
+
+  async function newUser(
+    name: string,
+    email: string,
+    phone: string,
+    birthDate: string,
+    role: string,
+    password: string,
+  ) {
+    if (isFormValid()) {
+      try {
+        await createUser({
+          variables: {
+            data: { name: name, email: email, phone: phone, birthDate: birthDate, password: password, role: role },
+          },
+        });
+        setCanRedirect(true);
+      } catch (error) {
+        alert(error);
+      }
     }
   }
   const today = new Date();
@@ -77,7 +84,7 @@ function AddUserPage(): JSX.Element {
       <header className='App-header'>
         <Link to='/'>Login</Link>
         <Link to={usersListPath}>Lista de Usuários</Link>
-        <form className='App-form' onSubmit={submitForm}>
+        <div className='App-form'>
           <h1 className='form-title'>Novo usuário</h1>
           <label>Nome</label>
           <input type='text' name='name' required className='Input' onChange={handleNameChange} />
@@ -95,15 +102,18 @@ function AddUserPage(): JSX.Element {
           />
           <label>E-mail</label>
           <input type='text' name='email' required className='Input' onChange={handleEmailChange} />
+          <label>Senha</label>
+          <input type='text' name='password' required className='Input' onChange={handlePasswordChange} />
           <label>Ocupação</label>
           <select name='role' required onChange={handleRoleChange}>
             <option value=''></option>
             <option value='user'>Usuário</option>
             <option value='admin'>Admin</option>
           </select>
-          <input type='submit' value='Adicionar usuário' className='Submit-button' />
-        </form>
+          <input type='submit' value='Adicionar usuário' className='Submit-button' onClick={submitForm} />
+        </div>
       </header>
+      {canRedirect && <Redirect to={usersListPath} />}
     </div>
   );
 }
